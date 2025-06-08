93268e88b6855f057f3b7d60a7e2e4c794058b05
Ricardo Andres <127223869+RicardoAndrex9@users.noreply.github.com>
2025-06-08 18:54:23 -0500

    refactor mixins, views, and admin modules for clarity and consistent formatting

--- applications/security/admin.py
+++ applications/security/admin.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-
 # security/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+
 from .models import User, Menu, Module, GroupModulePermission
+
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
@@ -12,11 +12,13 @@ class CustomUserAdmin(UserAdmin):
         ('Información adicional', {'fields': ('dni', 'image', 'direction', 'phone')}),
     )
 
+
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     list_display = ('name', 'icon', 'order')
     search_fields = ('name',)
     ordering = ('order', 'name')
+
 
 @admin.register(Module)
 class ModuleAdmin(admin.ModelAdmin):
@@ -24,6 +26,7 @@ class ModuleAdmin(admin.ModelAdmin):
     list_filter = ('menu', 'is_active')
     search_fields = ('name', 'url', 'description')
     ordering = ('menu', 'order', 'name')
+
 
 @admin.register(GroupModulePermission)
 class GroupModulePermissionAdmin(admin.ModelAdmin):
--- applications/security/views/home.py
+++ applications/security/views/home.py
@@ -1,19 +1,17 @@
-
-
 from django.views.generic import TemplateView
+
 from applications.security.components.menu_module import MenuModule
 from applications.security.components.mixin_crud import PermissionMixin
 
-class ModuloTemplateView(PermissionMixin,TemplateView):
+
+class ModuloTemplateView(PermissionMixin, TemplateView):
     template_name = 'home.html'
-   
+
     def get_context_data(self, **kwargs):
-        #context = super().get_context_data(**kwargs)
-        context={}
-        context["title"]= "IC - Modulos"
-        context["title1"]= "Modulos Disponibles"
+        # context = super().get_context_data(**kwargs)
+        context = {"title": "IC - Modulos", "title1": "Modulos Disponibles"}
         MenuModule(self.request).fill(context)
-        
+
         print("estoy saliendo en el modulo template view")
-       
+
         return context
